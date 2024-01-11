b7e8a2d9053a70583c2fb794bdfafb2212483340
Jeffrindh <156309474+Jeffrindh@users.noreply.github.com>
2024-01-11 16:00:02 +0530

    Java Assignments

---
+++ assign1.java
@@ -0,0 +1,18 @@
+package demo;
+import java.util.*;  
+public class assign1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		int num = sc.nextInt();
+		if(num>0){
+			System.out.println("The number is positive");
+		}
+		else{
+			System.out.println("The number is negative");
+		}
+	}
+
+}
